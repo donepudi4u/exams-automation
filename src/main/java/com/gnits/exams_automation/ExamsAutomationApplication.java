2554c80edb1b203eb1215e68c41570636845f409
donepudi4u <donepudi4u@gmail.com>
2025-04-17 14:33:21 -0400

    initial commit

---
+++ src/main/java/com/gnits/exams_automation/ExamsAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.gnits.exams_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gnits/exams_automation/ExamsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnits.exams_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
